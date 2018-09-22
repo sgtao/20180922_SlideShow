fbe1320722af96277d7a3bc6b668957359da6d50
Shogo Ogami <sg.tao.so@gmail.com>
2018-09-23 08:08:31 +0900

    add start/stop button

--- js/main.js
+++ js/main.js
@@ -1,10 +1,28 @@
-var slideIndex = 0;
-showSlides();
+'use strict'; // êÑèßíËã`
 
-function showSlides() {
+var isStarted = false;
+var start = document.getElementById('start');
+var stop  = document.getElementById('stop');
+
+var slideIndex = 0;
+showInitSlide();
+
+function showInitSlide() {
     var i;
     var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none";
+    }
+    slides[0].style.display = "block";
+
+}
+
+function showSlides() {
+    if (isStarted === false) {
+      return;
+    }
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
     for (i = 0; i < slides.length; i++) {
 	slides[i].style.display = "none";
     }
@@ -12,6 +30,27 @@ function showSlides() {
     slideIndex++;
     if (slideIndex > slides.length) {slideIndex = 1}
     slides[slideIndex-1].style.display = "block";
-    setTimeout(showSlides, 2000); // Change image every 2 seconds
 
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
+
+start.addEventListener('click', function(){
+    if (isStarted === true) {
+      return;
+    }
+    isStarted = true;
+    this.className = 'pushed';
+    stop.className = '';
+    showSlides();
+});
+
+stop.addEventListener('click', function(){
+    if (isStarted === false) {
+      return;
+    }
+    isStarted = false;
+    start.className = '';
+    this.className = 'pushed';
+    showSlides();
+});
+
